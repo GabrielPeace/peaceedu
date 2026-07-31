819e8e54d2a77c7aee60d761297631e055bde7d1
PeaceEdu <168727876+GabrielPeace@users.noreply.github.com>
2026-07-31 22:06:51 +0800

    bug fix

--- banner.js
+++ banner.js
@@ -2,7 +2,9 @@ const fs = require("fs");
 const pkg = require("./package.json");
 const filename = "assets/js/main.min.js";
 const script = fs.readFileSync(filename, "utf8");
-const banner = fs.readFileSync("_includes/copyright.js", "utf8");
+const banner = fs
+  .readFileSync("_includes/copyright.js", "utf8")
+  .replace(/\r\n?/g, "\n");
 
 if (!script.startsWith("/*!")) {
   fs.writeFileSync(filename, banner + script, "utf8");
